6cda5905b081625b4232c42abd21d3f53ab85499
Felipe Ribeiro <felipeboy_157@hotmail.com>
2019-10-07 16:39:23 -0300

    users save api

--- Backend/api/user.js
+++ Backend/api/user.js
@@ -1,6 +1,54 @@
+const bcrypt = require('bcrypt-nodejs')
+
 module.exports = app => {
-    const save = (req, res) => {
-        res.send('user save')
+    const { existsOrError, notExistsOrError, equalsOrError, nameMaxLenght } = app.api.validator
+
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
+    }
+
+    const save = async (req, res) => {
+        const user = { ...req.body }
+        if (req.params.id) user.id = req.params.id
+
+        if (!req.originalUrl.startsWith('/users')) user.admin = false
+        if (!req.user || !req.user.admin) user.admin = false
+
+        try {
+            existsOrError(user.name, 'Nome não informado!')
+            existsOrError(user.email, 'E-mail não informado!')
+            existsOrError(user.password, 'Senha não informada!')
+            existsOrError(user.confirmPassword, 'Senha Inválida!')
+            equalsOrError(user.password, user.confirmPassword, 'Senhas não compatíveis!')
+            nameMaxLenght(user.name, 'Tamanho máximo 20')
+
+            const userFromDB = await app.db('users')
+                .where({email: user.email}).first()
+            if (user.id) {
+                notExistsOrError(userFromDB, 'Usuário já cadastrado!')
+            }
+
+        } catch (msg) {
+            return res.status(400).send(msg)
+        }
+
+        user.password = encryptPassword(user.password)
+        delete user.confirmPassword
+
+        if (user.id) {
+            app.db('users')
+                .update(user)
+                .where({ id: user.id })
+                .whereNull('deletedAt')
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
+            app.db('users')
+                .insert(user)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
     }
 
     return { save }
