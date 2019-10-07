85348820a628f6ddf35d6ab02fe5ec719541417e
Felipe Ribeiro <felipeboy_157@hotmail.com>
2019-10-07 18:22:17 -0300

    Autenticar passport

--- Backend/config/routes.js
+++ Backend/config/routes.js
@@ -5,19 +5,23 @@ module.exports = app => {
     app.post('/validateToken', app.api.auth.validateToken)
 
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
 
     app.route('/users/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.user.save)
         .get(app.api.user.getById)
         .delete(app.api.user.remove)
 
     app.route('/activities')
+        .all(app.config.passport.authenticate())
         .get(app.api.activity.get)
         .post(app.api.activity.save)
 
     app.route('/activities/:id')
+        .all(app.config.passport.authenticate())
         .get(app.api.activity.getById)
         .put(app.api.activity.save)
         .delete(app.api.activity.remove)
