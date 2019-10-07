8afcdf399ca02171ed56a23498845922c4d6245d
Felipe Ribeiro <felipeboy_157@hotmail.com>
2019-10-07 17:37:44 -0300

    api activities +get +remove

--- Backend/api/activity.js
+++ Backend/api/activity.js
@@ -1,5 +1,5 @@
 module.exports = app => {
-    const {existsOrError, notExistsOrError, nameMaxLenght, idPositive} = app.api.validator
+    const {existsOrError, idPositive} = app.api.validator
 
     const save = (req, res) => {
         const activity = {...req.body}
@@ -7,7 +7,9 @@ module.exports = app => {
 
         try {
             existsOrError(activity.name, 'Nome não informado!')
-            nameMaxLenght(activity.name, 'Tamanho máximo 20')
+            existsOrError(activity.userId, 'Autor não informado!')
+            existsOrError(activity.description, 'Descrição não informada')
+            existsOrError(activity.status, 'Status não informado [pendente,fazendo,concluído]')
         } catch (msg) {
             return res.status(400).send(msg)
         }
@@ -26,7 +28,37 @@ module.exports = app => {
         }
     }
 
-    
+    const remove = async (req, res) => {
+        try {
+            existsOrError(req.params.id, 'Código da Atividade não informado!')
+            idPositive(req.params.id, 'Id deve ser maior que 0!')
 
-    return { save }
+            const rowsDeleted = await app.db('activities').where({id: req.params.id}).del()
+
+            try {
+                existsOrError(rowsDeleted, 'Ativitade não encontrado!')
+            } catch (msg) {
+                return res.status(400).send(msg)
+            }
+            res.status(204).send()
+        } catch (msg) {
+            res.status(500).send(msg)
+        }
+    }
+
+    const get = (req, res) => {
+        app.db('activities')
+            .then(activity => res.json(activity))
+            .catch(err => res.status(500).send(err))
+    }
+
+    const getById = (req, res) => {
+        app.db('activities')
+            .where({id: req.params.id})
+            .first()
+            .then(activity => res.json(activity))
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { save, get, getById, remove }
 }
